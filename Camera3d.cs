3dd5b9347cc311043ddcc6ef3d302eb3523d7be9
AnathemaKira <alexeidilger@yahoo.com>
2025-02-01 15:58:55 +0100

    formatting

--- Camera3d.cs
+++ Camera3d.cs
@@ -3,55 +3,55 @@ using System;
 
 public partial class Camera3d : Node3D
 {
-	[Export(PropertyHint.Range,"0,1")]
-	float movespeed = 0.2f;
+    [Export(PropertyHint.Range, "0,1")]
+    float movespeed = 0.2f;
 
-	[Export(PropertyHint.Range, "0,5")]
-	float zoomRange = 3f;
+    [Export(PropertyHint.Range, "0,5")]
+    float zoomRange = 3f;
 
-	Vector3 startingVector;
+    Vector3 startingVector;
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		startingVector = Transform.Origin;
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        startingVector = Transform.Origin;
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+    }
 
-	public override void _Input(InputEvent @event)
-	{
-	}
+    public override void _Input(InputEvent @event)
+    {
+    }
 
-	public override void _PhysicsProcess(double delta)
-	{
-		if (Input.IsActionPressed("move camera up 2D"))
-		{
-			TranslateObjectLocal(-Transform.Basis.Z * movespeed);
-		}
-		if (Input.IsActionPressed("move camera down 2D"))
-		{
-			TranslateObjectLocal(Transform.Basis.Z * movespeed);
-		}
-		if (Input.IsActionPressed("move camera right 2D"))
-		{
-			TranslateObjectLocal(Transform.Basis.X * movespeed);
-		}
-		if (Input.IsActionPressed("move camera left 2D"))
-		{
-			TranslateObjectLocal(-Transform.Basis.X * movespeed);
-		}
+    public override void _PhysicsProcess(double delta)
+    {
+        if (Input.IsActionPressed("move camera up 2D"))
+        {
+            TranslateObjectLocal(-Transform.Basis.Z * movespeed);
+        }
+        if (Input.IsActionPressed("move camera down 2D"))
+        {
+            TranslateObjectLocal(Transform.Basis.Z * movespeed);
+        }
+        if (Input.IsActionPressed("move camera right 2D"))
+        {
+            TranslateObjectLocal(Transform.Basis.X * movespeed);
+        }
+        if (Input.IsActionPressed("move camera left 2D"))
+        {
+            TranslateObjectLocal(-Transform.Basis.X * movespeed);
+        }
 
-		if (Input.IsActionPressed("zoom camera in") && Transform.Origin.Y >= startingVector.Y - zoomRange)
-		{
-			TranslateObjectLocal(-Transform.Basis.Y * movespeed);
-		}
-		if (Input.IsActionPressed("zoom camera out") && Transform.Origin.Y <= startingVector.Y)
-		{
-			TranslateObjectLocal(Transform.Basis.Y * movespeed);
-		}
-	}
+        if (Input.IsActionPressed("zoom camera in") && Transform.Origin.Y >= startingVector.Y - zoomRange)
+        {
+            TranslateObjectLocal(-Transform.Basis.Y * movespeed);
+        }
+        if (Input.IsActionPressed("zoom camera out") && Transform.Origin.Y <= startingVector.Y)
+        {
+            TranslateObjectLocal(Transform.Basis.Y * movespeed);
+        }
+    }
 }
