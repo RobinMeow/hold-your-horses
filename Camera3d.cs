4a0156c0331495a72eff19ed98ef7d475580c5a3
AnathemaKira <alexeidilger@yahoo.com>
2025-02-01 16:01:59 +0100

    conventions

--- Camera3d.cs
+++ Camera3d.cs
@@ -9,21 +9,11 @@ public partial class Camera3d : Node3D
     [Export(PropertyHint.Range, "0,5")]
     float zoomRange = 3f;
 
-    Vector3 startingVector;
+    Vector3 startingVector = Vector3.Zero;
 
-    // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         startingVector = Transform.Origin;
-    }
-
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-    }
-
-    public override void _Input(InputEvent @event)
-    {
     }
 
     public override void _PhysicsProcess(double delta)
