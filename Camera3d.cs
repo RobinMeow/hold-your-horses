4e0e7a238ba5c70c4291b759b14f2422b7bb5738
AnathemaKira <alexeidilger@yahoo.com>
2025-02-01 15:53:28 +0100

    Add camera zooming

--- Camera3d.cs
+++ Camera3d.cs
@@ -6,9 +6,15 @@ public partial class Camera3d : Node3D
 	[Export(PropertyHint.Range,"0,1")]
 	float movespeed = 0.2f;
 
+	[Export(PropertyHint.Range, "0,5")]
+	float zoomRange = 3f;
+
+	Vector3 startingVector;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		startingVector = Transform.Origin;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,11 +24,11 @@ public partial class Camera3d : Node3D
 
 	public override void _Input(InputEvent @event)
 	{
-
 	}
+
 	public override void _PhysicsProcess(double delta)
 	{
-		if(Input.IsActionPressed("move camera up 2D"))
+		if (Input.IsActionPressed("move camera up 2D"))
 		{
 			TranslateObjectLocal(-Transform.Basis.Z * movespeed);
 		}
@@ -37,6 +43,15 @@ public partial class Camera3d : Node3D
 		if (Input.IsActionPressed("move camera left 2D"))
 		{
 			TranslateObjectLocal(-Transform.Basis.X * movespeed);
+		}
+
+		if (Input.IsActionPressed("zoom camera in") && Transform.Origin.Y >= startingVector.Y - zoomRange)
+		{
+			TranslateObjectLocal(-Transform.Basis.Y * movespeed);
+		}
+		if (Input.IsActionPressed("zoom camera out") && Transform.Origin.Y <= startingVector.Y)
+		{
+			TranslateObjectLocal(Transform.Basis.Y * movespeed);
 		}
 	}
 }
